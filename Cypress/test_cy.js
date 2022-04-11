c9c4e3b3ffa9ff2f45604855fb93af4bc948e285
Chris C <50221322+ChrisC-sudo@users.noreply.github.com>
2022-04-11 13:31:11 -0400

    Add files via upload

---
+++ Cypress/test_cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress"/>
+
+it('google test', function(){
+    cy.visit('https://google.com')
+    cy.get('.gLFyf').type('iphone 13 pro max')
+    cy.get('.CqAVzb > center > .gNO89b').click()
+
+
+})
