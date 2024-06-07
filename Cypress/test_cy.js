5562493a61c740cad55f350ed7044b52dc00f942
Chris Christie <cchristie035@gmail.com>
2024-06-07 13:39:23 -0400

    cypress init

--- Cypress/test_cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress"/>
-
-it('google test', function(){
-    cy.visit('https://google.com')
-    cy.get('.gLFyf').type('Apple')
-    cy.get('.CqAVzb > center > .gNO89b').click()
-
-
-})
