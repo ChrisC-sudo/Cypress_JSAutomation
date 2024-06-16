7786b23621007fab5d2607ea0598dbc3bdc46c2f
Chris Christie <cchristie035@gmail.com>
2024-06-16 19:18:19 -0400

    actions cypress testing

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config)
+
       // implement node event listeners here
+      return config
     },
     specPattern: 'cypress/e2e/integration/*.js'
   },
